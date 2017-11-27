0666b29e0cfb016147867528e185ab26e466393d
timcliffe <timothy.h.cliffe@gmail.com>
2017-11-27 14:21:54 -0600

    derrrr

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -18,10 +18,33 @@ public class SearchController {
 
     @RequestMapping(value = "")
     public String search(Model model) {
-        model.addAttribute("columns", ListController.columnChoices);
-        return "search";
+       model.addAttribute("columns", ListController.columnChoices);
+       return "search";
     }
 
     // TODO #1 - Create handler to process search request and display results
+    @RequestMapping(value = "results")
+    public String search(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        ArrayList<HashMap<String, String>> jobs;
+        if (searchType.equals("all") || searchTerm.equals(""))  {
+            jobs = JobData.findByValue(searchTerm);
+        }
+        else {
+            jobs = JobData.findByColumnAndValue(searchTerm, searchType);
+        }
+
+        //System.out.println("I wanna crash.....");
+        //System.out.flush();
+
+
+        model.addAttribute("columns", ListController.columnChoices);
+        model.addAttribute("jobs", jobs);
+        //model.addAttribute("tim", "is in the library");
+
+
+
+
+        return "search";
+    }
 
 }
